0c271b7f9b71d6244bab6f7d237513bd411c6700
Giovanna Bernadelli de Souza Goes <94538745+Gigii55@users.noreply.github.com>
2025-04-13 14:58:37 -0300

    Create DescobertaMetodos.java

---
+++ DescobertaMetodos.java
@@ -0,0 +1,23 @@
+package atividade;
+
+import java.util.Random;
+
+public class DescobertaMetodos {
+
+	String embaralharPalavras (String palavraSelecionada){
+		
+		 char[] palavraArray = palavraSelecionada.toCharArray();
+		 Random random = new Random();
+		 
+		 for(int x=0; x<palavraArray.length; x++){
+			 
+          int indiceAleatorio = random.nextInt(palavraArray.length);
+          char temp = palavraArray[x];
+          palavraArray[x] = palavraArray[indiceAleatorio];
+          palavraArray[indiceAleatorio] = temp;
+      }
+
+      return new String(palavraArray); 
+}
+	
+}
