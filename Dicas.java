2eb5f6714ab2940a43f378d68d98d281f9f80ada
Giovanna Bernadelli de Souza Goes <94538745+Gigii55@users.noreply.github.com>
2025-04-13 15:16:52 -0300

    Update Dicas.java

--- Dicas.java
+++ Dicas.java
@@ -7,34 +7,47 @@ public class Dicas {
 	void tentativas ( String palavraSelecionada) {
 		
 		Scanner scanner = new Scanner (System.in);
-		String palavraChute = scanner.nextLine();
 		
 		int tentativas = 1;
 		
 	    Boolean acertou = false;
 	    
+	    
+	    
+	    
 	    while (true) { 
-            System.out.println("Informe a palavra, 'dica' para receber uma dica ou '0 para desistir'");
-            String entrada = scanner.nextLine();
-            if(entrada.equals("0")) {
-                System.out.println("Frangote desistiu com " + tentativas + " tentativas");
-                break;
-            }
+	    	
+	    	 String tenta = scanner.nextLine();
+	    	 
+	    	 if(tenta.equals(palavraSelecionada)){
+	    		 
+	                System.out.println("Você acertou com " + tentativas + " tentativas");
+	                acertou = true;
+	                break;
+	            }
 
-            if(entrada.equals("dica")) {
-                System.out.println("A primeira letra é '" + palavraSelecionada.charAt(0) + "', a última é '" + palavraSelecionada.charAt(palavraSelecionada.length() - 1) + "'");
-                continue;
-            }
+	            else{
+	            	
+	                tentativas++;
+	                
+	                System.out.println("Informe a palavra, 'dica' para receber uma dica ou '0 para desistir'");
+	                String entrada = scanner.nextLine();
+	                
+	                if(entrada.equals("0")) {
+	                    System.out.println("Frangote desistiu com " + tentativas + " tentativas");
+	                    break;
+	                }
 
-            if(entrada.equals(palavraSelecionada)){
-                System.err.println("Você acertou com " + tentativas + " tentativas");
-                acertou = true;
-                break;
-            }
+	                if(entrada.equals("dica")) {
+	                    System.out.println("A primeira letra é '" + palavraSelecionada.charAt(0) + "', a última é '" + palavraSelecionada.charAt(palavraSelecionada.length() - 1) + "'");
+	                    continue;
+	                }
+	            }
+	    	
+	    	
+           
 
-            else{
-                tentativas++;
-            }
+           
         }
 	
 }
