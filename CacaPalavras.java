67b33eac750004c476bedecfd812e77e0747f7e1
Giovanna Bernadelli de Souza Goes <94538745+Gigii55@users.noreply.github.com>
2025-04-23 22:32:35 -0300

    Update CacaPalavras.java

--- CacaPalavras.java
+++ CacaPalavras.java
@@ -4,29 +4,36 @@ public class CacaPalavras {
 
 	public static void main(String[] args) {
 		
-
-		
 		SelecionarPalavras palavra = new SelecionarPalavras ();
-		PreencheTabuleiro preenche = new PreencheTabuleiro();	
+		PreencheTabuleiro preenche = new PreencheTabuleiro();
 		ColocaPalavraNoTabuleiro coloca = new ColocaPalavraNoTabuleiro ();
-		ImprimirTabuleiro imprime = new ImprimirTabuleiro ();		
-		
+		ImprimirTabuleiro imprime = new ImprimirTabuleiro ();	
+		Dicas dica = new Dicas();
+		DicaParaDuasPalavras dica2 = new DicaParaDuasPalavras ();
 		
 		char tabuleiro [] [] = new char [10][10];
 		
 		String palavraSelecionada = palavra.selecionarPalavra();
+		String palavraSelecionada2 = palavra.selecionarPalavra();
 		
 		char[][] p = preenche.preencheTabuleiro(tabuleiro);
 		
-		palavra.selecionarPalavra();
-		preenche.preencheTabuleiro(tabuleiro);
+		
 		coloca.colocaPalavra(palavraSelecionada, p);
+		coloca.colocaPalavra(palavraSelecionada2, p);
+		
 		imprime.imprimirTabuleiro(tabuleiro);
-	
+		
+		System.out.println("Qual a palavra 1? ");
+		
+		dica.tentativas(palavraSelecionada);
+		
+		System.out.println("Qual a palavra 2? ");
+		
+		dica2.tentativas(palavraSelecionada2);
 		
 		
 }
 	
-	
-	
+
 	}
